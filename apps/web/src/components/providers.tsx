617727fba523ee7ea72041bcd2f4a58b8affa519
sunniekapar <kaparsunnie@gmail.com>
2025-08-13 14:50:41 -0700

    deps: add nuqs

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { NuqsAdapter } from "nuqs/adapters/next";
 import { Toaster } from "./ui/sonner";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <NuqsAdapter>
       {children}
       <Toaster richColors />
-    </>
+    </NuqsAdapter>
   );
 }
